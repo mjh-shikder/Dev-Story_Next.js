d9cf76c862593822f428522fdc741e65b7b3b529
mjh-shikder <mjhsikder@gmail.com>
2025-12-31 20:48:27 +0600

    Group Route added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,29 @@
 import Banner from "../components/Banner";
 import Image from "next/image";
+import axios from "axios";
 
 export default function Home() {
+
+  // [product, setProduct] = useState([])
+  
+  // useEffect(() => {
+  //   axios.get(
+  //     "[https://a-10-backend-paw-mart-web-app.vercel.app/listing](https://a-10-backend-paw-mart-web-app.vercel.app/listing)"
+  //   ).then(result => {
+      
+  //   })
+  // },[])
+
   return (
     <div>
       <h2 className="text-4xl ">Welcome to Dev-Story 🦈 </h2>
       <Banner></Banner>
+      <h1 className="bg-linear-60 from-sky-50 to-sky-200 p-3 rounded text-gray-700 text-center font-bold text-2xl my-5 ">
+        Product
+      </h1>
+      <div>
+
+      </div>
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
     return (
         <div className='text-center space-y-5 bg-linear-60 to-sky-100 from-sky-400 text-black p-10  '>
             <h2 className='text-4xl font-bold '>Welcome to Dev story </h2>
-            <button onClick={handleBtn} className='px-4 py-2 bg-sky-500 hover:bg-sky-600 text-white rounded-xl cursor-pointer'>Share Story</button>
+            <button onClick={handleBtn} className='px-4 py-2 bg-sky-500 hover:bg-sky-600 duration-1000 text-white rounded-xl cursor-pointer'>Share Story</button>
         </div>
     );
 };
