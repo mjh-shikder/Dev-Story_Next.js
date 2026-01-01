0ba247af6a3862063520cfaf96e093f0eeb8e002
mjh-shikder <mjhsikder@gmail.com>
2026-01-01 23:49:44 +0600

    Getting the Params

--- src/app/tutorials/[technology]/[topic]/pages/[page_no]/[subject]/page.jsx
+++ src/app/tutorials/[technology]/[topic]/pages/[page_no]/[subject]/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const TutorialPage = () => {
+const TutorialPage = async ({ params }) => {
+    
+    const {technology, topic, page_no, subject} = await params;
+    
+    console.log({ technology, topic, page_no, subject });
+
+
     return (
         <div>
             This is Tutorial Page
