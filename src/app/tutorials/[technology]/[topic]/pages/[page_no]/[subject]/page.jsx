0e3a40b58562ac9f7231a535fb8e659390630e69
mjh-shikder <mjhsikder@gmail.com>
2026-01-01 23:45:23 +0600

    Tutorial page Dynamic segment added

---
+++ src/app/tutorials/[technology]/[topic]/pages/[page_no]/[subject]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TutorialPage = () => {
+    return (
+        <div>
+            This is Tutorial Page
+        </div>
+    );
+};
+
+export default TutorialPage;
